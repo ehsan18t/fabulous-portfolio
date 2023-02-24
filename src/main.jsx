09e65055ab3fef56bdc8c145e2c5df2339c5b2eb
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-02-24 12:38:04 +0600

    TailwindCSS Integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
-import './assets/styles/index.css'
+import './assets/styles/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
