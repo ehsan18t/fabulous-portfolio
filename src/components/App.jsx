924bfbbf6efa082a31cf3f1b6f37c09ba6c9cb81
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-02-24 14:16:44 +0600

    added eslint and pretter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,4 @@
-import { useState } from 'react'
-import reactLogo from '../assets/img/react.svg'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="flex flex-col h-screen justify-center">
       <div className="flex justify-center">
