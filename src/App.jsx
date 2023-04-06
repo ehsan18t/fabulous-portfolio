89134b557de4ea0295f5156072a6ea294ae63e66
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-04-07 00:54:47 +0600

    added routes for new pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Home from './pages/home/Home'
 import About from './pages/about/About'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Nav from './components/Nav/Nav'
+import Projects from './pages/projects/Projects'
+import Blog from './pages/blog/Blog'
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home title="Home Page" />} />
           <Route path="/about" element={<About title="About Page" />} />
+          <Route path="/projects" element={<Projects title="Projects" />} />
+          <Route path="/blog" element={<Blog title="Blog" />} />
         </Routes>
       </BrowserRouter>
     </>
